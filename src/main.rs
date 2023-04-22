af21d410a29ccc5dfba692cd520eac545f7777c0
shreyas-shriyan <shreyshriyan@gmail.com>
2023-04-22 23:50:34 +0530

    added add numbers function

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
 // This is the main function.
 fn main() {
-    hello_world()
+    // hello_world()
+    add_numbers()
 }
 
-fn hello_world() {
-    println!("Hello World")
+// fn hello_world() {
+//     println!("Hello World")
+// }
+
+fn add_numbers() {
+    let a: i32=2;
+    let b: i32=2;
+
+    let sum: i32 = a+b;
+    
+    println!("The sum is {sum}")
 }
