4a86ba9be99ffce688227f279aafeb8e233ff67e
shreyas-shriyan <shreyshriyan@gmail.com>
2023-04-23 23:27:04 +0530

    added for loop

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,23 @@
 // This is the main function.
 fn main() {
     // hello_world()
-    add_numbers()
+    // add_numbers()
+    for_loop()
 }
 
 // fn hello_world() {
 //     println!("Hello World")
 // }
 
-fn add_numbers() {
-    let a: i32=2;
-    let b: i32=2;
+// fn add_numbers() {
+//     let a: i32 = 2;
+//     let b: i32 = 2;
+//     println!("The sum is {}", a + b)
+// }
 
-    let sum: i32 = a+b;
-    
-    println!("The sum is {sum}")
+fn for_loop(){
+    // =can be used to include end number
+    for x in 1..=5{
+        println!("the loop is {x}")
+    }
 }
